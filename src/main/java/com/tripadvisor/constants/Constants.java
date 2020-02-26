d8b4df30cf5daed8cd69725a1cd97a66d54d6104
Aditi Bhalerao <aditi.sys@gmail.com>
2020-02-26 09:09:52 +0530

    removed hardcoded paths

--- src/main/java/com/tripadvisor/constants/Constants.java
+++ src/main/java/com/tripadvisor/constants/Constants.java
@@ -2,9 +2,9 @@ package constants;
 
 public class Constants {
 	
-	public static final String PROPERTIESFILE = "E:\\Workspace\\Tripadvisor\\src\\main\\java\\com\\tripadvisor\\resources\\config.properties";
-	public static final String CHROMEDRIVER ="E:\\Workspace\\Tripadvisor\\chromedriver.exe";
-	public static final String GEKODRIVER ="E:\\Workspace\\Tripadvisor\\chromedriver.exe";
+	public static final String PROPERTIESFILE = System.getProperty("user.dir")+"\\src\\main\\java\\com\\tripadvisor\\resources\\config.properties";
+	public static final String CHROMEDRIVER =System.getProperty("user.dir")+"\\chromedriver.exe";
+	public static final String GEKODRIVER =System.getProperty("user.dir")+"\\chromedriver.exe";
 	public static final int IMPLICITWAIT = 10;
 	public static final int EXPLICITWAIT = 30;
 	public static final String ATTRIBUTE ="data-value";
